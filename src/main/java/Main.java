d336bfb893348c4bb7ec2f58f89caa6a5db3ce8d
WhiteKIM <9rudrb40@naver.com>
2026-03-06 19:10:38 +0900

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Set;
 
 public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final Set<String> supportCommandSet = Set.of();
+
     public static void main(String[] args) throws Exception {
          System.out.print("$ ");
+         String commnad = br.readLine();
+
+         if(!supportCommandSet.contains(commnad)) {
+             bw.write(commnad + ": " + "command not found");
+         }
+
+         bw.flush();
+         bw.close();
+         br.close();
     }
 }
