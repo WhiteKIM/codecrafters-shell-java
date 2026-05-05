f7f7917ffd7416a6a6b3fb3993e3e09dc5877686
WhiteKIM <9rudrb40@naver.com>
2026-05-05 21:07:48 +0900

    codecrafters submit [skip ci]

--- src/main/java/command/impl/EchoCommand.java
+++ src/main/java/command/impl/EchoCommand.java
@@ -2,6 +2,7 @@ package command.impl;
 
 import command.ShellCommand;
 import command.utils.handler.ParserHandler;
+import command.utils.parser.BackSlashParser;
 import command.utils.parser.BasicParser;
 import command.utils.parser.DoubleQuoteParser;
 import command.utils.parser.SingleQuoteParser;
@@ -10,12 +11,14 @@ public class EchoCommand implements ShellCommand<String, String> {
     @Override
     public String process(String input) {
         ParserHandler basicHandler = new ParserHandler(new BasicParser());
+        ParserHandler backslashHandler = new ParserHandler(new BackSlashParser());
         ParserHandler singleQuoteHandler = new ParserHandler(new SingleQuoteParser());
         ParserHandler doubleQuoteHandler = new ParserHandler(new DoubleQuoteParser());
 
+        backslashHandler.setHandler(basicHandler);
         basicHandler.setHandler(singleQuoteHandler);
         singleQuoteHandler.setHandler(doubleQuoteHandler);
 
-        return basicHandler.run(input);
+        return backslashHandler.run(input);
     }
 }
---
+++ src/main/java/command/utils/parser/BackSlashParser.java
@@ -0,0 +1,64 @@
+package command.utils.parser;
+
+import java.util.Set;
+
+public class BackSlashParser implements QuoteParser {
+    private final Set<Character> specialWord = Set.of(
+                    '\'', '\\', '\"', '$', '#', '?', '.', ',', '|',
+                    '&', '*', '(', ')'
+    );
+
+    // CASE
+    // backslash space between
+
+    // backslash combine other space
+
+    // backslash with special word
+
+    @Override
+    public String process(String input) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < input.length(); i++) {
+            char atChar = input.charAt(i);
+
+            if(atChar != '\\') {
+                sb.append(atChar);
+            } else {
+                if(i + 1 < input.length()) {
+                    char nextChar = input.charAt(i + 1);
+
+                    // 특수 문자
+                    if(specialWord.contains(nextChar)) {
+                        sb.append(nextChar);
+                        i += 1;
+                    } else if(nextChar == ' ') {// 공백
+                        for(int j = i + 1; j < input.length(); j++) {
+                            if(input.charAt(j) != ' ') {
+                                sb.append(' ').append(input.charAt(j - 1));
+                                i = j;  // 위치 갱신
+                                break;
+                            } 
+                        }
+                    } else {
+                        // 다음 문자는 일반 문자열
+                        continue;
+                    }
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+
+    @Override
+    public boolean isSupport(String input) {
+        for(int i = 0; i < input.length(); i++) {
+            if(input.charAt(i) == '\\') {
+                return true;
+            }
+        }
+
+        return true;
+    }
+}
